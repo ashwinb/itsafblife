9e081ba12ec0625c5f320b692dcd0fb34653a0ea
Elliot Lynde <elynde@ubuntu.(none)>
2011-01-12 14:29:42 -0800

    increased bitrate for better quality...try it out, it's getting pretty cool\!

--- cmd.sh
+++ cmd.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-export VBITRATE=5000
 export SOUNDFILE=../green_day.mp3
 
+# the python creates a folder named $user_id and calls this script with
+# that user id as a param
 cd $1
 
-# resize the images
+# resize, center and fill images
 for img in *.jpg
   do convert $img -resize 720x480 -background black -gravity center -extent 720x480 $img
 done
@@ -17,11 +18,6 @@ mencoder mf://@files.txt \
 -audiofile ${SOUNDFILE} \
 -ovc lavc \
 -oac lavc \
--lavcopts vcodec=mpeg4 \
+-lavcopts vcodec=mpeg4:vbitrate=5000 \
 -ofps 30000/1001 \
 -o $1.mpeg
-
-# mencoder -ovc lavc -oac lavc -lavcopts \
-#   vcodec=mpeg2video:vrc_buf_size=1835:vrc_maxrate=9800:vbitrate=${VBITRATE}:keyint=18:acodec=ac3:abitrate=192:aspect=4/3:trell:mbd=2:dia=4:cmp=3:precmp=3:ildctcmp=3:subcmp=3:mbcmp=3:cbp:mv0:dc=10 \
-#   -of mpeg -mpegopts format=dvd -vf expand=720:480,harddup \
-#   -srate 48000 -af lavcresample=48000 -ofps 30000/1001 -o ${MPG_2} ${MPG_1}
